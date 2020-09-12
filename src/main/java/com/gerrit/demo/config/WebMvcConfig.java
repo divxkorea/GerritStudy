70897be1d318602ca2c19b26995cdaa86a7c384a
James Kim <divxkorea@gmail.com>
2020-09-12 20:21:44 +0900

    Init

--- src/main/java/com/gerrit/demo/config/WebMvcConfig.java
+++ src/main/java/com/gerrit/demo/config/WebMvcConfig.java
@@ -2,18 +2,34 @@ package com.gerrit.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
+	
+	@Override
+	public void configureDefaultServletHandling( DefaultServletHandlerConfigurer configurer ) {
+		configurer.enable();
+	}
+	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
 		internalResourceViewResolver.setPrefix( "/WEB-INF/views/" );
 		internalResourceViewResolver.setSuffix( ".jsp" );
 		return internalResourceViewResolver;
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//registry.addResourceHandler("/resources/**").addResourceLocations("/resources/", "classpath:/static/");
+		registry.addResourceHandler( "/resources/**" ).addResourceLocations( "/resources/" ).setCachePeriod( 31556926 );
 	}
 }
--- src/main/webapp/WEB-INF/views/home/demo.jsp
+++ src/main/webapp/WEB-INF/views/home/demo.jsp
@@ -2,6 +2,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script type="text/javascript" src="/resources/static/js/jquery-3.5.1.js"></script>
+<script type="text/javascript">
+
+	$(function(){
+		alert("테스트");
+	});
+</script>
 <title>데모</title>
 </head>
 <body>
